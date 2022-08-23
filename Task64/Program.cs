4c89dc5e2d7535c3378cd999aab45a4d1df6a745
Alex Belyaev <t433621@yandex.ru>
2022-08-23 16:51:42 +0300

    рекурсия от M до N

---
+++ Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+M = 4; N = 8. -> ""4, 6, 7, 8"" 
+*/
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return NumbersRec(a, b - 1) + $"{b}, ";
+    else return string.Empty;
+
+}
+
+Console.WriteLine(NumbersRec(4, 8));
